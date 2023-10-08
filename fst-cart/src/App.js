e0b763b9985070be29f2e1f901a8aacf82404727
mdmainuddin360 <115131966+mdmainuddin360@users.noreply.github.com>
2023-10-08 13:59:16 +0530

    code added

--- fst-cart/src/App.js
+++ fst-cart/src/App.js
@@ -2,7 +2,23 @@ import './App.css';
 
 function App() {
   return (
-  <h1>WelCome to <b>FSTMART</b></h1>
+    <div className="app-container">
+      <nav className="navbar">
+        <span className="logo">FSTMART</span>
+      </nav>
+      <div className="content">
+        <h1>Welcome to <b>FSTMART</b></h1>
+        <div className="slogans">
+          <p>Building and Painting, Simplified.</p>
+          <p>Quality Supplies, Endless Possibilities.</p>
+          <p>Creating with Confidence.</p>
+          <p>Your Building and Painting Partner.</p>
+          <p>Turning Ideas into Reality.</p>
+          <p>FSTMART: Your Creative Catalyst.</p>
+          <p>Bringing Vision to Vibrant Life.</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
